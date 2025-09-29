fa1fe87244591e7baae2557fad96d29553735e4f
Nihad005 <nikoquluzade5@gmail.com>
2025-09-29 07:52:20 +0000

    Changes

---
+++ Lab3/Lab3_1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int array_min(int arr[], int size) {
+    if (size <= 0) return 0;
+    int min = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+int array_max(int arr[], int size) {
+    if (size <= 0) return 0;
+    int max = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+int array_sum(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+float array_avg(int arr[], int size) {
+    if (size <= 0) return 0.0f;
+    int sum = array_sum(arr, size);
+    return (float)sum / size;
+}
+
+int main() {
+    int arr[] = {10, 20, 5, 30, 15};
+    int size = 5;
+    printf("Min: %d\n", array_min(arr, size));
+    printf("Max: %d\n", array_max(arr, size));
+    printf("Sum: %d\n", array_sum(arr, size));
+    printf("Avg: %.2f\n", array_avg(arr, size));
+    return 0;
+}
